474b0c2cf52570a70dc717c29b92b9138161da08
Paulo Cerdeira <paulocerdeira1999@gmail.com>
2022-04-26 18:41:41 -0300

    fixes

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/buin/env sh
+
+set -e
+
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'New Deployment'
+git push -f https://github.com/PauloCerdeira/github-pages-test.git gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: '/github-pages-test/',
+  publicPath: '/github-pages-test/',
   transpileDependencies: true
 })
